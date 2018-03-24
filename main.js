f465cae907a4352add5e8eb6b2733356bd89e83e
chandu <chandrasr.palle@gmail.com>
2018-03-24 19:56:35 +0100

    background color change animation

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ let x=2;
 let y=4;
 z=x+y;
 
+setTimeout("document.bgColor='white'", 1000)
+setTimeout("document.bgColor='lightpink'", 1500)
+setTimeout("document.bgColor = 'pink'", 2000)
+setTimeout("document.bgColor =  'deeppink'", 2500)
+setTimeout("document.bgColor = 'red'", 3000)
+setTimeout("document.bgColor = 'tomato'", 3500)
+setTimeout("document.bgColor = 'darkred'", 4000)
 
 $(document).ready(function(){
     $("h1").text(typeof z);
