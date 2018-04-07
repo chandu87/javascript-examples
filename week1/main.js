68a57aaf5e8945afd6f5f221e7384a5605bb27e3
chandu <chandrasr.palle@gmail.com>
2018-04-07 20:22:44 +0200

    examples for this variable and its scope

--- week1/main.js
+++ week1/main.js
@@ -6,13 +6,13 @@ let x=2;
 let y=4;
 z=x+y;
 
-setTimeout("document.bgColor='white'", 1000)
-setTimeout("document.bgColor='lightpink'", 1500)
-setTimeout("document.bgColor = 'pink'", 2000)
-setTimeout("document.bgColor =  'deeppink'", 2500)
-setTimeout("document.bgColor = 'red'", 3000)
-setTimeout("document.bgColor = 'tomato'", 3500)
-setTimeout("document.bgColor = 'darkred'", 4000)
+// setTimeout("document.bgColor='white'", 1000)
+// setTimeout("document.bgColor='lightpink'", 1500)
+// setTimeout("document.bgColor = 'pink'", 2000)
+// setTimeout("document.bgColor =  'deeppink'", 2500)
+// setTimeout("document.bgColor = 'red'", 3000)
+// setTimeout("document.bgColor = 'tomato'", 3500)
+// setTimeout("document.bgColor = 'darkred'", 4000)
 
 setTimeout("document.f1.ta1.value = 'Answer Soon'", 1000)
 setTimeout("document.f1.ta1.value = ''", 1300)
@@ -30,3 +30,55 @@ setTimeout("document.f3.ta6.value = ''", 4300)
 $(document).ready(function(){
     $("h1").text(typeof z);
 });
+
+var years=[2001,1992,1993,1994];
+var emptyArray = [];
+
+for(var i = 0;i < years.length; i++){
+
+    emptyArray[i] = 2018 - years [i];
+}
+console.log(years);
+console.log(emptyArray);
+for (var i = 0; i < emptyArray.length; i++){
+    if(emptyArray[i] >= 18)
+    {
+        console.log("person born in "+years[i]+"s fullage now"+ emptyArray[i] )
+    }
+    else{
+        console.log("person born in "+years[i]+"s is not fullage now"+ emptyArray[i] )
+    }
+}
+function printFullAge(years){
+    var ages = [];
+    var output = [];
+    for(var i = 0;i < years.length; i++){
+        ages[i] = 2018 - years [i];
+        if(ages[i] >= 18){
+         output[i] = true;   
+        }
+        else{
+            output[i]=false;
+        }
+    }
+return output;
+}
+console.log(printFullAge(years));
+
+// this varible scope
+
+console.log(this);
+rasmus();
+function rasmus(){
+    console.log(this);
+}
+//above it is refering to global object
+
+var janaki = {
+        name : "hana",
+        age: 343,
+        calc: function(){
+            console.log(this);
+        }
+};
+janaki.calc();
