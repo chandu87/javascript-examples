201195b76ac8aff522e1733c926f7d1c448105d6
chandu <chandrasr.palle@gmail.com>
2018-04-10 23:42:11 +0200

    toggle classList

---
+++ eventListeners/toggle/toggle.js
@@ -0,0 +1,9 @@
+var button = document.getElementsByTagName("button");
+// button[0].addEventListener('click',function(){
+//     document.body.style.background = "red";
+// });
+
+button[0].addEventListener('click',function(){
+    document.body.classList.toggle("bColor");
+});
+
