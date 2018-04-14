e7c77735d3ef37947e91937e0f56ddc9a3045e50
chandu <chandrasr.palle@gmail.com>
2018-04-14 16:37:49 +0200

    objects and references passing

--- Advanced-JS/script.js
+++ Advanced-JS/script.js
@@ -26,3 +26,20 @@ console.log(obj2.age);
 obj2.name = "jen";
 console.log(obj1.name);
 console.log(obj2.name);
+
+var age = 20;
+var fruits = {
+    apples :1,
+    mangos :3
+};
+
+function changeObj(a,b){
+    a = 30;
+    b.mangos = 20;
+}
+changeObj(age,fruits);
+//when you pass primitivs only value passed not the reference
+console.log(age);
+
+//reference is passed when you pass object so the value changes
+console.log(fruits.mangos);
