8886906c21649ea319a666ee1cf52be2b6b9682a
chandu <chandrasr.palle@gmail.com>
2018-04-22 14:36:45 +0200

    mouseover event listener added

--- week5/exercise1.js
+++ week5/exercise1.js
@@ -1,13 +1,13 @@
 console.log("HI excersie 1 ====>");
 
-let ulItems = document.querySelector("#items");
+let ulItems = document.querySelector("ul#items");
 let btn = document.querySelector("#btn");
 let count = 1;
 
 btn.addEventListener('click',addListItem);
+btn.addEventListener('mouseover',addListItem);
 
 function addListItem(){
-
     let li = document.createElement('li');
     li.textContent= "Item " + count++;
     ulItems.appendChild(li);
