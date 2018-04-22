2019457a65b2a4329fed401c90c258a9ddc43af6
chandu <chandrasr.palle@gmail.com>
2018-04-22 14:24:57 +0200

    added count

--- week5/exercise1.js
+++ week5/exercise1.js
@@ -1,13 +1,15 @@
-console.log("HI excersie 1 ------->");
+console.log("HI excersie 1 ====>");
 
 let ulItems = document.querySelector("#items");
 let btn = document.querySelector("#btn");
+let count = 1;
 
 btn.addEventListener('click',addListItem);
 
 function addListItem(){
+
     let li = document.createElement('li');
-    li.textContent= "New List Item";
+    li.textContent= "Item " + count++;
     ulItems.appendChild(li);
 }
 
