72dd7899a01019b5165216018d4770ceca94c295
chandu <chandrasr.palle@gmail.com>
2018-05-27 13:18:16 +0200

    initial commit

---
+++ week10_revision/main.js
@@ -0,0 +1,22 @@
+console.log("HI");
+
+
+var x = 5;
+
+function test2() {
+
+  function test1() { // -> linked to test2 scope (not linked to the window/global/main/ something anymore)
+    const varInsideTest1 = 'test1';
+    //debugger;
+    console.log(x);
+    console.log(varInsideTest2);
+  }
+
+  const varInsideTest2 = 'test2';
+  return test1;
+}
+
+var t = test2();
+t();
+
+console.log(varInsideTest2);
